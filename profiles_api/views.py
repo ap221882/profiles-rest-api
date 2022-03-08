c216cf85bb89270f1a14def3a0e5d95f927742c0
Ajay Pathak <ap221882@gmail.com>
2022-03-08 19:22:15 +0530

    Added all methods to APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,13 @@
+from email import message
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 class HelloAPIView(APIView):
+ serializers_class=serializers.HelloSerializer
+
  def get(self,request,format=None):
   """Returns a list of APIView features"""
   an_apiview=[
@@ -12,3 +18,28 @@ class HelloAPIView(APIView):
   ]
 
   return Response({'message':'Hello', 'an_apiview':an_apiview})
+
+ def post(self,request):
+  serializer=self.serializers_class(data=request.data)
+
+  if serializer.is_valid():
+   name=serializer.validated_data.get('name')
+   message=f'Hello {name}'
+   return Response({'message':message})
+  else:
+   return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+ 
+
+ def put(self,request,pk=None):
+  """Updating"""
+  return Response({'method':'PUT'})
+
+ def patch(self,request,pk=None):
+  """PArtial Updating"""
+  return Response({'method':'PATCH'})
+
+  
+ def delete(self,request,pk=None):
+  """Deletion"""
+  return Response({'method':'DELETE'})
+
