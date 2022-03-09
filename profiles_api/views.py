cef1337c23dabd1d4c9566d8be3a99a9a72a7a0d
Ajay Pathak <ap221882@gmail.com>
2022-03-09 13:12:10 +0530

    Added Searching

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,10 +1,13 @@
 # from email import message
+from encodings import search_function
 from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from profiles_api import permissions
+from rest_framework import filters
+
 
 from profiles_api import serializers
 from profiles_api import models
@@ -82,3 +85,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
  queryset=models.UserProfile.objects.all()
  authentication_classes=(TokenAuthentication,)
  permission_classes=(permissions.UpdateOwnProfile,)
+ filter_backends=(filters.SearchFilter,)
+ search_fields=('name','email',)
