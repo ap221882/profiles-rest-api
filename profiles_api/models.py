49a2ec9c7fe73f8327f6597b9820b6d9bbff00ce
Ajay Pathak <ap221882@gmail.com>
2022-03-09 14:22:52 +0530

    Added Login API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
+from django.conf import settings
 
 # Create your models here.
 class UserProfileManager(BaseUserManager):
@@ -43,3 +44,5 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     
     def __str__(self):
         return self.email
+
+# class ProfileFee
